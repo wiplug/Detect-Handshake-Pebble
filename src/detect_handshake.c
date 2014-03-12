f2b8a8958282acfe0b331e8d32b365d3fbe54bfb
chrislgarry <chrislgarry@gmail.com>
2014-03-12 19:37:15 -0400

    FFT implemented, but Pebble SDK missing required standard c dependencies for FFT: #include <stdlib.h>

--- src/detect_handshake.c
+++ src/detect_handshake.c
@@ -1,5 +1,21 @@
 #include <pebble.h>
+#include "kiss_fftr.h"
 
+/*
+PEBBLE LIB DOES NOT CONTAIN THE FOLLOWING REQUIRED DEPENDENCIES:
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+
+*/
+
+#define WINDOW_SIZE 14
+#define SAMPLE_RATE ACCEL_SAMPLING_10HZ
+
+ 
 //For displaying content on the watch face.
 //Although the window might not be used to display content, its declaration is required.
 Window *my_window;
@@ -9,9 +25,33 @@ TextLayer *text_layer;
 
 //This handler will be called when enough samples have been received to fill one batch of data
 void accel_data_handler(AccelData *data, uint32_t num_samples) {
-  //Compute DFT. If peak frequency between 1 and 4Hz (2.5Hz avg), display text "Handshake Detected" for 3 seconds.
-    AccelData datatemp = *data;
-    uint32_t temp = num_samples;
+
+        //Compute FFT
+
+            //Determine the computationally optimal array size for the input signal
+            int OPTIMAL_SIZE =  kiss_fft_next_fast_size((int)num_samples);
+            
+        /*  THE FOLLOWING CODE HAS STANDARD DEPENDENCIES THAT PEBBLE 
+            DOES NOT INCLUDE IN THE SDK WHICH WE NEED:
+            
+            //Allocate memory for the variables and set configuration vars
+            //0=forward transform, NULL,NULL=do not malloc
+            //kiss_fftr_cfg configBuffer = kiss_fftr_alloc(OPTIMAL_SIZE, 0, 0, 0);
+            kiss_fft_cfg  configBuffer = kiss_fft_alloc(OPTIMAL_SIZE, 0, 0, 0);
+
+            //Store AccelData into const kiss_fft_scalar *timedata for FFT
+            const kiss_fft_scalar timedata[OPTIMAL_SIZE]; //TODO: store data;
+
+            //Create output array
+            kiss_fft_cpx freqdata[(OPTIMAL_SIZE/2)+1];
+
+            //Compute FFT
+            //kiss_fftr(configBuffer,timedata,freqdata);
+
+        */
+
+        //If peak frequency between 1.5 and 4Hz (2.5Hz avg), display "Handshake Detected" for 3 seconds.
+            //...code
 }
 
 //Initialization of parameters for the mobile app
@@ -31,12 +71,12 @@ void handle_init(void) {
   
     //Subscribe/initialize the batch accelerometer data processing with 14 samples 
     //per batch and pass to accel_data_handler for processing
-    accel_data_service_subscribe(14, &accel_data_handler);
+    accel_data_service_subscribe(WINDOW_SIZE, &accel_data_handler);
   
     //Set accelerometer sampling rate to 10Hz (important for window size).
     //If sampling rate is increased, then batch size must be increased to 
     //capture whole handshake within the window.s
-    accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+    accel_service_set_sampling_rate(SAMPLE_RATE);
   
   //TODO: Can peek at accelerometer service for individual value. Good for detecting onset of handshake.
 }
