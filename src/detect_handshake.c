c2b0d4b29bbfbcefb34d8767d3b4a106efdbae98
chrislgarry <chrislgarry@gmail.com>
2014-03-23 02:01:55 -0400

    finished DTW. App crashes when DTW is called.

--- src/detect_handshake.c
+++ src/detect_handshake.c
@@ -8,15 +8,15 @@
  *              or the Fast Fourier Transform (FFT).
  *              Prints a message to the display when a Pebble Smart Watch 
  *              user engages a handshake.
- */
+*/
 
 #include <pebble.h>
 #include "fft_lib/kiss_fftr.h" //FOR FFT
 
 #define WINDOW_SIZE 14 //Accerometer sample window size to capture a full handshake
 #define SAMPLE_RATE ACCEL_SAMPLING_10HZ
-#define DIMENSION 30   //FOR DTW: Dimension of the accelerometer space
-#define MAX_DISTANCE 1 //FOR DTW: Threshold distance between reference handshake and candidate
+#define DIMENSION 3   //FOR DTW: Dimension of the accelerometer space
+#define MAX_DISTANCE 500000//FOR DTW: Threshold distance between reference handshake and candidate
 #define DTW 0
 #define FFT 1
 #define MODE DTW //Choose gesture recognition technique
@@ -27,7 +27,13 @@
 Window *my_window;
 
 //A renderable graphics element. Content is rendered on a particular layer within the window.
-TextLayer *text_layer;
+Layer *layer;
+
+//Handshake detected flag
+int IS_HANDSHAKE;
+
+//Current distance between handshake signature and accelerometer sample
+int distance = 4000000000;
 
 // FFT LIBRARY:
     
@@ -35,9 +41,27 @@ TextLayer *text_layer;
 
 // DTW LIBRARY:
 
-    //The reference handshake to compare accelerometer data to for DTW
     int **handshake_ref;
-    int handshake_ref_size;
+
+    //The reference handshake to compare accelerometer data to for DTW
+    int handshake_signature[14][3] = {
+		{-417,427,-76},
+		{-398,531,13},
+		{-393,548,126},
+		{-506,150,34},
+		{-531,929,-372},
+		{-350,1053,97},
+		{-24,126,-197},
+		{-158,-430,-114},
+		{ 14,15,-181},
+		{-267,1028,87},
+		{ 1,607,-31},
+		{-29,464,-45},
+		{-27,443,-1},
+		{-9,459,-51}
+    };
+
+    int handshake_ref_size = WINDOW_SIZE;
 
     //FOR DTW: Allocate space for accelerometer data matrix (each row is an [x,y,z])
     int** allocAccBuf(int len){
@@ -56,7 +80,7 @@ TextLayer *text_layer;
         free(p);
     }
 
-    //FOR DTW: Dynamic Time Warping Distance: Return integer distance between two discrete time signals.
+    //FOR DTW: Dynamic Time Warping Distance: Return integer distance between two discrete time signals (close ~ 10k, dissimilar ~500k).
     int DTWdistance(int **sample1, int length1, int **sample2, int length2, int i, int j, int *table) {
         
         if( i < 0 || j < 0)
@@ -106,7 +130,7 @@ TextLayer *text_layer;
 //This handler will be called when enough samples have been received to fill one batch of data
 void accel_data_handler(AccelData *data, uint32_t num_samples) {
 
-    int i, j, distance, *table, **sample, OPTIMAL_SIZE;
+    int i, j, *table, **sample;//, OPTIMAL_SIZE;
 
     switch(MODE){
 
@@ -127,23 +151,28 @@ void accel_data_handler(AccelData *data, uint32_t num_samples) {
                         for( j = 0; j < WINDOW_SIZE*handshake_ref_size; j++)
                             table[j] = -1;
 
+                    /*Causing peble app crash
                     //Compute DTW Distance between candidate sample and reference
-                        distance = DTWdistance(sample,WINDOW_SIZE, handshake_ref, handshake_ref_size,WINDOW_SIZE-1,handshake_ref_size-1, table);
+                        distance = DTWdistance(sample,WINDOW_SIZE,handshake_ref,handshake_ref_size,
+                        	WINDOW_SIZE-1,handshake_ref_size-1,table);
+					*/
 
                 //If DTW Distance between candidate sample and reference is small, handshake detected
                     
                     //TODO: Determine optimal MAX_DISTANCE
-                    if(distance < MAX_DISTANCE){
-                        //print handshake detected for three seconds, then clear screen
-                    }
+	                    IS_HANDSHAKE = 0;
+	                    if(distance < MAX_DISTANCE){
+	                    	IS_HANDSHAKE = 1;
+	                    }
 
                     //Clean up
                         free(table);
                         releaseAccBuf(sample, WINDOW_SIZE);
                         releaseAccBuf(handshake_ref, WINDOW_SIZE);
+
             break;
 
-        case FFT: 
+        case FFT: //Currently not working and commented out. Necessary libraries missing.
             //FFT
                 //Compute ONE-DIMENSIONAL REAL FFT
 
@@ -187,21 +216,57 @@ void accel_data_handler(AccelData *data, uint32_t num_samples) {
 
 }
 
+static void update_layer_callback(Layer *layer, GContext* ctx) {
+
+  graphics_context_set_text_color(ctx, GColorBlack);
+  GRect bounds = layer_get_frame(layer);
+
+  if(IS_HANDSHAKE){
+	  graphics_draw_text(ctx,
+	      "Handshake!",
+	      fonts_get_system_font(FONT_KEY_FONT_FALLBACK),
+	      GRect(5, 5, bounds.size.w-10, 100),
+	      GTextOverflowModeWordWrap,
+	      GTextAlignmentLeft,
+	      NULL);
+	}
+	else{
+	  graphics_draw_text(ctx,
+	      "No Handshake.",
+	      fonts_get_system_font(FONT_KEY_FONT_FALLBACK),
+	      GRect(90, 100, bounds.size.w-95, 60),
+	      GTextOverflowModeWordWrap,
+	      GTextAlignmentRight,
+	      NULL);
+	}
+}
+
 //Initialization of parameters for the mobile app
 void handle_init(void) {
-  
+  	//Initialize handshake flag
+	IS_HANDSHAKE = 0;
+
     //Create a window to display content
 	my_window = window_create();
+    
     //Push window onto the window stack
     window_stack_push(my_window, true /* true means use default window load animation */);
+    
     //Create a rendering layer for window
     Layer *window_layer = window_get_root_layer(my_window);
+    
     //Get window layer dimensions/bounds
     GRect bounds = layer_get_frame(window_layer);
   
     //Create a text layer
-    text_layer = text_layer_create((GRect){ .origin = { 0, 30 }, .size = bounds.size });
-  
+    layer = layer_create(bounds);
+  	
+  	//Set rendering function for text layer
+  	layer_set_update_proc(layer, update_layer_callback);
+  	
+  	//Add text layer to the window
+  	layer_add_child(window_layer, layer);
+
     //Subscribe/initialize the batch accelerometer data processing with 14 samples 
     //per batch and pass to accel_data_handler for processing
     accel_data_service_subscribe(WINDOW_SIZE, &accel_data_handler);
@@ -212,16 +277,22 @@ void handle_init(void) {
     accel_service_set_sampling_rate(SAMPLE_RATE);
 
     if(MODE == DTW){
-        //Initialize reference handshake
-        handshake_ref = allocAccBuf(WINDOW_SIZE);
-        handshake_ref_size = WINDOW_SIZE;
-    } 
-  
+	    //Initialize handshake signature
+	    handshake_ref = allocAccBuf(WINDOW_SIZE);
+
+	    //Populate reference handshake matrix pointer
+	    int z;
+	    for(z=0; z<WINDOW_SIZE;z++){
+	    	handshake_ref[z][0] = handshake_signature[z][0];
+	    	handshake_ref[z][1] = handshake_signature[z][1];
+	    	handshake_ref[z][2] = handshake_signature[z][2];
+	    }
+  	}
 }
 
 //Deinitialization (mem free, etc.) of initialization parameters upon quit 
 void handle_deinit(void) {
-	  text_layer_destroy(text_layer);
+	  layer_destroy(layer);
 	  window_destroy(my_window);
       accel_data_service_unsubscribe();
 }
